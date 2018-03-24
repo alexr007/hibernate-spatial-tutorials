8b20dea55d297159367f491265c57526e9a8213c
Karel Maesen <karel@geovise.com>
2018-03-24 14:46:55 +0100

    Cleanup code
    Conflicts:
    	hs5-event-tutorial/src/main/java/event/EventManager.java

--- hs5-event-tutorial/src/main/java/event/Event.java
+++ hs5-event-tutorial/src/main/java/event/Event.java
@@ -1,62 +1,81 @@
 package event;
 
-import com.vividsolutions.jts.geom.Point;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Type;
-
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.Date;
+
+import com.vividsolutions.jts.geom.Point;
+
+import org.hibernate.annotations.GenericGenerator;
 
 /**
  * @author Karel Maesen, Geovise BVBA
- *         creation-date: 6/18/12
+ * creation-date: 6/18/12
  */
 @Entity
 public class Event {
 
-        @Id
-        @GeneratedValue(generator="increment")
-        @GenericGenerator(name="increment", strategy = "increment")
-        private Long id;
+	@Id
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy = "increment")
+	private Long id;
 
-        private String title;
+	private String title;
 
-        private Date date;
+	private Date date;
 
-        private Point location;
+	private Point location;
 
-        public Event() {
-        }
+	public Event() {
+	}
 
-        public Long getId() {
-            return id;
-        }
 
-        private void setId(Long id) {
-            this.id = id;
-        }
+	public Event(String title, Date theDate, Point geom) {
+		this.title = title;
+		this.date = theDate;
+		this.location = geom;
+	}
 
-        public Date getDate() {
-            return date;
-        }
+	public Long getId() {
+		return id;
+	}
 
-        public void setDate(Date date) {
-            this.date = date;
-        }
+	private void setId(Long id) {
+		this.id = id;
+	}
 
-        public String getTitle() {
-            return title;
-        }
+	public Date getDate() {
+		return date;
+	}
 
-        public void setTitle(String title) {
-            this.title = title;
-        }
+	public void setDate(Date date) {
+		this.date = date;
+	}
 
-        public Point getLocation() {
-            return this.location;
-        }
+	public String getTitle() {
+		return title;
+	}
 
-        public void setLocation(Point location) {
-            this.location = location;
-        }
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Point getLocation() {
+		return this.location;
+	}
+
+	public void setLocation(Point location) {
+		this.location = location;
+	}
+
+	@Override
+	public String toString() {
+		return "Event{" +
+				"id=" + id +
+				", title='" + title + '\'' +
+				", date=" + date +
+				", location=" + location +
+				'}';
+	}
 }
